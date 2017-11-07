217a0d93747b6f1af255761af9670912e417861d
0xbrock <0xbrock@users.noreply.github.com>
2017-11-06 22:34:16 -0500

    Add Player prefab, coin prefab, game manager #5 #6

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,26 @@
+﻿using Character;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    public int coinValue = 1;
+    public bool taken = false;
+
+    // if the player touches the coin, it has not already been taken, and the player can move (not dead or victory)
+    // then take the coin
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((other.tag == "Player") && (!taken) && (other.gameObject.GetComponent<PlatformerCharacter2D>().playerCanMove))
+        {
+            // mark as taken so doesn't get taken multiple times
+            taken = true;
+            
+            // do the player collect coin thing
+            other.gameObject.GetComponent<PlatformerCharacter2D>().CollectCoin(coinValue);
+
+            // destroy the coin
+            DestroyObject(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager gm;
+
+    public int coins = 0;
+
+    // set things up here
+    void Awake()
+    {
+        // setup reference to game manager
+        if (gm == null)
+            gm = this.GetComponent<GameManager>();
+    }
+
+    public void AddPoints(int amount)
+    {
+        coins = amount;
+    }
+}
